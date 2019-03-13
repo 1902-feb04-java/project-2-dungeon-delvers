576c4bf55684c7903dea91310653fc0221f5469f
SpencerPulliam <spencer_pulliam@yahoo.com>
2019-03-12 19:53:22 -0500

    Basic character sheet.

---
+++ player/src/character/App.java
@@ -0,0 +1,11 @@
+package character;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(SheetFactory.Grimlock.characterName 
+				+ " attacks with his Greataxe for " 
+				+ SheetFactory.Grimlock.damageDie + " damage!");
+	}
+
+}
