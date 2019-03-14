7336a5b2fa78c59be231c6b2306429594eafb099
Adam King <adamdentonking@gmail.com>
2019-03-14 10:18:48 -0500

    deleted dnd folder bc java files already in src

--- dnd/src/character/SheetFactory.java
+++
@@ -1,9 +0,0 @@
-package character;
-
-import character.Sheet;
-import dice.DiceFactory;
-
-public class SheetFactory {
-	static Sheet Grimlock = new Sheet("Spencer","Grimlock Graybeard", 50, 14, DiceFactory.d8 );
-
-}
