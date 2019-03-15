8cbee3c14d6cc814d9253945e46935264fb5f8b8
Adam King <adamdentonking@gmail.com>
2019-03-14 21:09:48 -0500

    basic POSTing between angular app and spring

--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendText(s:string): Observable<Object> {
+    return this.http.post("/register2", s, { responseType: 'text'});
+  }
 }
