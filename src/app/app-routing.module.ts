b1f7c40381f8fe28800720c1371f3c2eb91b5010
SpencerPulliam <spencer_pulliam@yahoo.com>
2019-03-15 11:30:52 -0500

    basic character creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { CampaignComponent } from './campaign/campaign.component';
+import { CharacterComponent } from './character/character.component';
+import { CharacterCreationComponent } from './character-creation/character-creation.component';
+
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'campaign', component: CampaignComponent}
+  {path: 'campaign', component: CampaignComponent},
+  {path: 'character', component: CharacterComponent},
+  {path: 'pccreation', component: CharacterCreationComponent}
 ];
 
 @NgModule({
