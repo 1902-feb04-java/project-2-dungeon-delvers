7a23c6dad736f38063a24a43609f8902ae8b91e7
John Vasgird <38052305+chec13@users.noreply.github.com>
2019-03-20 13:03:29 -0500

    fixed sockets

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as Stomp from 'stompjs';
+import * as Stomp from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
 
 @Component({
@@ -22,20 +22,24 @@ export class ChatWindowComponent implements OnInit {
 
   initializeWebSocketConnection() {
     let ws = new SockJS(this.serverUrl);
-    this.stompClient = Stomp.over(ws);
+    this.stompClient = Stomp.Stomp.over(ws);
     let that = this;
     this.stompClient.connect({}, function(frame) {
       that.stompClient.subscribe("/topic/public", (message) => {
         if (message.body) {
           console.log(message.body);
-          this.chatStrings.push(message.body);
+          
         }
       });
     });
   }
 
   sendMessage(message) {
-    this.stompClient.send("/app/chat.sendMessage", {}, message);
+    
+    
+    let obj = {type: 0, message, sender: 'me'};
+    this.stompClient.send("/app/chat.sendMessage", null, JSON.stringify(obj) );
+    console.log('sending message to' + this.stompClient.serverUrl);
   }
 
 }
--- src/main/java/DD/WebSocketEventListener.java
+++ src/main/java/DD/WebSocketEventListener.java
@@ -1,4 +1,4 @@
-package DD;
+ package DD;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
