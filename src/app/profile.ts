aacb5253258f988c6b5a0ce5cd5c52969a788a63
John Vasgird <38052305+chec13@users.noreply.github.com>
2019-03-21 16:48:44 -0500

    account and profile work in service now

--- src/app/profile.ts
+++ src/app/profile.ts
@@ -4,18 +4,18 @@ export class Profile
 {
     characters:string[]; //needs to be of character type.
     campaigns:Campaign[]; //needs to be of type Campaign.
-    encounters:string[]; // needs to be of type encounter.
+    monsters:string[]; // needs to be of type encounter.
     status:boolean; // false == offline, true == online
     username:string;
     id:number;
-    password:string;
-    constructor(ch:string[], ca:Campaign[], e:string[], s:boolean, u:string, id:number, password:string = ""){
+   
+    constructor(ch:string[], ca:Campaign[], m:string[], s:boolean, u:string, id:number){
         this.characters = ch;
         this.campaigns = ca;
-        this.encounters = e;
+        this.monsters = m;
         this.status = s;
         this.username = u;
         this.id = id;
-        this.password = password;
+       
     }
 }
