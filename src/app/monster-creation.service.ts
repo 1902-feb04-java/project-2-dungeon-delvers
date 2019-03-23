29b129444c30542df8386ab66aef5e81a6162d7b
John Vasgird <38052305+chec13@users.noreply.github.com>
2019-03-23 16:24:33 -0500

    bugs

--- src/app/monster-creation.service.ts
+++ src/app/monster-creation.service.ts
@@ -3,15 +3,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Monster } from './monster';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonsterCreationService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private loginService:LoginService) { }
 
   sendMonster(m:Monster): Observable<any> {
+    m.account_id = this.loginService.myAccount.id;
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     console.log(m.toString());
